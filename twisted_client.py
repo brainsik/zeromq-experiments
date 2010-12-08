c31397e425b0dfd931f8c70995a7e3c99f9c5fbe
Eric Allen <ericpallen@gmail.com>
2010-12-07 23:53:05 -0800

    Repeatedly ping to stress test

--- twisted_client.py
+++ twisted_client.py
@@ -2,16 +2,14 @@
 
 from twisted.internet import reactor, protocol
 
-
 class TwistedClient(protocol.Protocol):
-
     def connectionMade(self):
         print "twisted: sending ping"
         self.transport.write("ping")
 
     def dataReceived(self, data):
         print "twisted: received", data
-        reactor.stop()
+        reactor.callLater(0, self.connectionMade)
 
 
 factory = protocol.ClientFactory()
